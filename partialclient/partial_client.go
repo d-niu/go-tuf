b26e5d55c874fbbad525357c55680e1656285415
D Niu <88739846+d-niu@users.noreply.github.com>
2022-01-20 13:31:14 -0500

    Custom local/remote stores for partial client, variety of update methods, new test fixtures

--- partialclient/partial_client.go
+++ partialclient/partial_client.go
@@ -1,8 +1,10 @@
 package partialclient
 
 import (
+	"encoding/json"
 	"github.com/theupdateframework/go-tuf/client"
 	"github.com/theupdateframework/go-tuf/data"
+	"io"
 	"sync"
 )
 
@@ -10,8 +12,8 @@ type PartialClient struct {
 	sync.Mutex
 
 	rootClient  *client.Client
-	localStore  client.LocalStore
-	remoteStore client.RemoteStore //*partialClientRemoteStore
+	localStore  partialClientLocalStore
+	remoteStore partialClientRemoteStore
 
 	valid bool
 
@@ -21,10 +23,42 @@ type PartialClient struct {
 	//targetFiles []*pbgo.File //pbgo is dataadog specific so it's probably not waht we want
 }
 
+type partialClientLocalStore struct {
+}
+
+func (l *partialClientLocalStore) SetMeta(name string, meta json.RawMessage) error {
+	return nil
+}
+
+func (l *partialClientLocalStore) GetMeta() (map[string]json.RawMessage, error) {
+	return nil, nil
+}
+
+func (l *partialClientLocalStore) DeleteMeta(name string) error {
+	return nil
+}
+
+func (l *partialClientLocalStore) Close() error {
+	return nil
+}
+
+type partialClientRemoteStore struct {
+}
+
+func (r *partialClientRemoteStore) GetMeta(name string) (stream io.ReadCloser, size int64, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (r *partialClientRemoteStore) GetTarget(path string) (stream io.ReadCloser, size int64, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
 //NewPartialClient will create a partial client
-func NewPartialClient(local client.LocalStore, remote client.RemoteStore, rootVersion uint64) (*PartialClient, error) {
+func NewPartialClient(local partialClientLocalStore, remote partialClientRemoteStore, rootVersion uint64) (*PartialClient, error) {
 	c := &PartialClient{
-		rootClient:  client.NewClient(local, remote),
+		rootClient:  client.NewClient(&local, &remote),
 		localStore:  local,
 		remoteStore: remote,      //do we need a custom one?
 		rootVersion: rootVersion, //get the meta repo's RootsDirector's last version : check with info
@@ -32,12 +66,27 @@ func NewPartialClient(local client.LocalStore, remote client.RemoteStore, rootVe
 	return c, nil
 }
 
-//Update the partial client by updating roots, root versions, and validating and updating targets
-func (c *PartialClient) Update() error {
+//PartialUpdateAll Updates the partial client by updating roots (the partial way), root versions, and validating and updating targets
+func (c *PartialClient) PartialUpdateAll() error {
+	return nil
+}
+
+//FullUpdateAll Updates the partial client by updating the roots (the full way)
+func (c *PartialClient) FullUpdateAll() error {
+	return nil
+}
+
+//PartialUpdate Updates the partial client by updating roots (the partial way), root versions, and validating and updating targets
+func (c *PartialClient) PartialUpdate(targets data.TargetFiles) error {
+	return nil
+}
+
+//FullUpdate Updates the partial client by updating the roots (the full way)
+func (c *PartialClient) FullUpdate(targets data.TargetFiles) error {
 	return nil
 }
 
 //Targets returns the current targets (5.4.4.1 Partial Verification)
 func (c *PartialClient) Targets() {
-	return
+
 }
--- partialclient/partial_client_test.go
+++ partialclient/partial_client_test.go
@@ -11,21 +11,37 @@ func Test(t *testing.T) { TestingT(t) }
 
 //Mirroring the setup for the full verification client
 type PartialClientSuite struct {
-	store       tuf.LocalStore
+	store       partialClientLocalStore
 	repo        *tuf.Repo
-	local       tuf.LocalStore
-	remote      *fakeRemoteStore //make one
+	local       partialClientLocalStore
+	remote      partialClientRemoteStore
 	expiredTime time.Time
 	keyIDs      map[string][]string
 }
 
-type fakeRemoteStore struct {
-}
-
-func newFakeRemoteStore() *fakeRemoteStore {
-	return &fakeRemoteStore{}
-}
-
+//Test errors that should be detected upon initializing any partial client
 func (s *PartialClientSuite) TestInit(c *C) {
 
 }
+
+//Test partial (as defined in Uptane) updating all targets
+func (s *PartialClientSuite) TestPartialUpdateAll() {
+
+}
+
+//Test full (as defined in TUF) updating all targets
+func (s *PartialClientSuite) TestFullUpdateAll() {
+
+}
+
+//Test partial (as defined in Uptane) updating some targets
+func (s *PartialClientSuite) TestPartialUpdate() {
+
+}
+
+//Test full (as defined in TUF) updating some targets
+func (s *PartialClientSuite) TestFullUpdate() {
+
+}
+
+//TODO: test valid/invalid rotations, missing/incorrect target files
